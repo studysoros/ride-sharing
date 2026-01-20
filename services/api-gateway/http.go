68ebe7e160d14fd35bcde8c7779eb3a1a96e8c4f
fatsummercat <homelnw77@gmail.com>
2026-01-20 18:49:53 +0700

    feat: implement trip preview handler and HTTP server integration

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"ride-sharing/shared/contracts"
 )
@@ -21,7 +23,24 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := contracts.APIResponse{Data: "ok"}
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
 
-	writeJSON(w, http.StatusCreated, res)
+	res, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	var resBody any
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
+		return
+	}
+
+	response := contracts.APIResponse{Data: resBody}
+
+	writeJSON(w, http.StatusCreated, response)
 }
