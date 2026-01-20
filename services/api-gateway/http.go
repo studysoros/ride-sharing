e993a2d1766453c330cb3aa1562be850b33acc1f
fatsummercat <homelnw77@gmail.com>
2026-01-20 18:12:35 +0700

    feat: implement an http server

---
+++ services/api-gateway/http.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"ride-sharing/shared/contracts"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var reqBody previewTripRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	if reqBody.UserID == "" {
+		http.Error(w, "user ID is required", http.StatusBadRequest)
+		return
+	}
+
+	res := contracts.APIResponse{Data: "ok"}
+
+	writeJSON(w, http.StatusCreated, res)
+}
