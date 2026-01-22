0e673da308695c3b322cfa4e70de855883f6dead
fatsummercat <homelnw77@gmail.com>
2026-01-22 11:10:07 +0700

    feat: implement grpc client on api gateway

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"ride-sharing/services/api-gateway/grpc_clients"
 	"ride-sharing/shared/contracts"
 )
 
@@ -25,6 +26,13 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 
 	jsonBody, _ := json.Marshal(reqBody)
 	reader := bytes.NewReader(jsonBody)
+
+	tripService, err := grpc_clients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer tripService.Close()
 
 	res, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
 	if err != nil {
