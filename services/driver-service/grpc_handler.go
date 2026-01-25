7785201df87a64c6dc45c6c765c2722ca943f774
fatsummercat <homelnw77@gmail.com>
2026-01-25 11:00:23 +0700

    feat: implement driver registration and unregistration via grpc

--- services/driver-service/grpc_handler.go
+++ services/driver-service/grpc_handler.go
@@ -24,9 +24,22 @@ func NewGrpcHandler(s *grpc.Server, service *Service) {
 }
 
 func (h *driverGrpcHandler) RegisterDriver(ctx context.Context, req *pb.RegisterDriverRequest) (*pb.RegisterDriverResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RegisterDriver not implemented")
+	driver, err := h.service.RegisterDriver(req.GetDriverID(), req.GetPackageSlug())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to register driver")
+	}
+
+	return &pb.RegisterDriverResponse{
+		Driver: driver,
+	}, nil
 }
 
 func (h *driverGrpcHandler) UnregisterDriver(ctx context.Context, req *pb.RegisterDriverRequest) (*pb.RegisterDriverResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UnregisterDriver not implemented")
+	h.service.UnregisterDriver(req.GetDriverID())
+
+	return &pb.RegisterDriverResponse{
+		Driver: &pb.Driver{
+			Id: req.GetDriverID(),
+		},
+	}, nil
 }
--- services/driver-service/service.go
+++ services/driver-service/service.go
@@ -1,6 +1,13 @@
 package main
 
-import pb "ride-sharing/shared/proto/driver"
+import (
+	math "math/rand/v2"
+	pb "ride-sharing/shared/proto/driver"
+	"ride-sharing/shared/util"
+	"sync"
+
+	"github.com/mmcloughlin/geohash"
+)
 
 type driverInMap struct {
 	Driver *pb.Driver
@@ -8,10 +15,52 @@ type driverInMap struct {
 
 type Service struct {
 	drivers []*driverInMap
+	mu      sync.RWMutex
 }
 
 func NewService() *Service {
 	return &Service{
 		drivers: make([]*driverInMap, 0),
+	}
+}
+
+func (s *Service) RegisterDriver(driverId string, packageSlug string) (*pb.Driver, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	randomIndex := math.IntN(len(PredefinedRoutes))
+	randomRoute := PredefinedRoutes[randomIndex]
+
+	randomPlate := GenerateRandomPlate()
+	randomAvatar := util.GetRandomAvatar(randomIndex)
+
+	// this property must be sent to the frontend
+	geohash := geohash.Encode(randomRoute[0][0], randomRoute[0][1])
+
+	driver := &pb.Driver{
+		Id:             driverId,
+		Geohash:        geohash,
+		Location:       &pb.Location{Latitude: randomRoute[0][0], Longitude: randomRoute[0][1]},
+		Name:           "Lando Norris",
+		PackageSlug:    packageSlug,
+		ProfilePicture: randomAvatar,
+		CarPlate:       randomPlate,
+	}
+
+	s.drivers = append(s.drivers, &driverInMap{
+		Driver: driver,
+	})
+
+	return driver, nil
+}
+
+func (s *Service) UnregisterDriver(driverId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for i, driver := range s.drivers {
+		if driver.Driver.Id == driverId {
+			s.drivers = append(s.drivers[:i], s.drivers[i+1:]...)
+		}
 	}
 }
